296d6ce76780a47d8f32c9ee97d6dba1555e64d8
Steven Wu <master_msp1@hotmail.com>
2021-06-22 19:42:36 +0200

    text-link-image mobile done

---
+++ components/TextLinkImage.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="container text-link-image__container" v-editable="blok">
+    <div class="text-link-image__copy">
+      <h2 class="text-link-image__title">{{ blok.title }}</h2>
+      <p class="text-link-image__description">{{ blok.description }}</p>
+      <NuxtLink :to="blok.link_url.cached_url" class="link text-link-image__link">{{ blok.link_text }}</NuxtLink>
+    </div>
+    <figure class="text-link-image__figure">
+      <img class="text-link-image__image" :src="blok.image.filename" :alt="blok.image.alt" />
+    </figure>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    blok: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style>
+</style>
