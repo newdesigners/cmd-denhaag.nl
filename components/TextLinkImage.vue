96baf2d366eac125a52e76e9e31071a40024ea59
Steven Wu <master_msp1@hotmail.com>
2021-06-29 21:02:24 +0200

    fixed issue with creating new components

--- components/TextLinkImage.vue
+++ components/TextLinkImage.vue
@@ -3,9 +3,9 @@
     <div class="text-link-image__copy">
       <h2 class="text-link-image__title">{{ blok.title }}</h2>
       <p class="text-link-image__description">{{ blok.description }}</p>
-      <NuxtLink :to="blok.link_url.cached_url" class="link text-link-image__link">{{ blok.link_text }}</NuxtLink>
+      <NuxtLink v-if="blok.link_url" :to="blok.link_url.cached_url" class="link text-link-image__link">{{ blok.link_text }}</NuxtLink>
     </div>
-    <figure class="text-link-image__figure">
+    <figure class="text-link-image__figure" v-if="blok.image">
       <img class="text-link-image__image" :src="blok.image.filename" :alt="blok.image.alt" />
     </figure>
   </div>
