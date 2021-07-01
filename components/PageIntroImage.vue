f426b49e85b409f750b6e0529fa52eeb377cae1a
Steven Wu <master_msp1@hotmail.com>
2021-07-01 02:17:29 +0200

    added page intro image edition

---
+++ components/PageIntroImage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container page-intro__container" v-editable="blok">
+    <h1 class="page-intro__title">{{ blok.title }}</h1>
+    <div class="page-intro__copy">
+      <p class="page-intro__paragraph">{{ blok.text_left }}</p>
+      <p class="page-intro__paragraph">{{ blok.text_right }}</p>
+    </div>
+    <div class="page-intro__media">
+      <figure class="page-intro__image">
+        <img v-if="blok.image" :src="blok.image.filename" :alt="blok.image.alt" class="page-intro__image-src" />
+      </figure>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    blok: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style>
+</style>
