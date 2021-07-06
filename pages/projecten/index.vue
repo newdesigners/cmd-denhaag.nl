77e13980512bf7d8ede0d99be1ac0086930158ed
Steven Wu <master_msp1@hotmail.com>
2021-07-06 05:09:47 +0200

    added more pages

--- pages/projecten/index.vue
+++ pages/projecten/index.vue
@@ -1,14 +1,81 @@
 <template>
   <section>
+    <component
+      v-if="story.content.component"
+      :key="story.content._uid"
+      :blok="story.content"
+      :is="story.content.component"
+    />
   </section>
 </template>
- 
+
 <script>
 export default {
+  async fetch(context) {
+    // Loading reference data - Stories in our case
+    if(context.store.state.stories.loaded !== '1') {
+      let storiesRefRes = await context.app.$storyapi.get(`cdn/stories/`, { starts_with: 'verhalen/', version: 'draft' });
+      context.store.commit('stories/setStories', storiesRefRes.data.stories);
+      context.store.commit('stories/setLoaded', '1');
+    }
+
+    if(context.store.state.projects.loaded !== '1') {
+      let projectsRefRes = await context.app.$storyapi.get(`cdn/stories/`, { starts_with: 'projecten/', version: 'draft' });
+      context.store.commit('projects/setProjects', projectsRefRes.data.stories);
+      context.store.commit('projects/setLoaded', '1');
+    }
+  },
+  data () {
+    return {
+      story: { content: {} }
+    }
+  },
+  mounted () {
+    this.$storybridge(() => {
+      const storyblokInstance = new StoryblokBridge();
+ 
+      // Use the input event for instant update of content
+      storyblokInstance.on('input', (event) => {
+        console.log(this.story.content);
+        if (event.story.id === this.story.id) {
+          this.story.content = event.story.content;
+        }
+      })
+ 
+      // Use the bridge to listen the events
+      storyblokInstance.on(['published', 'change'], (event) => {
+        // window.location.reload()
+        this.$nuxt.$router.go({
+          path: this.$nuxt.$router.currentRoute,
+          force: true,
+        })
+      })
+    })
+  },
+  asyncData (context) {
+    // // This what would we do in real project
+    // const version = context.query._storyblok || context.isDev ? 'draft' : 'published'
+    // const fullSlug = (context.route.path == '/' || context.route.path == '') ? 'home' : context.route.path
+ 
+    // Load the JSON from the API - loadig the home content (index page)
+    return context.app.$storyapi.get('cdn/stories/projecten', {
+      version: 'draft'
+    }).then((res) => {
+      return res.data;
+    }).catch((res) => {
+      if (!res.response) {
+        console.error(res);
+        context.error({ statusCode: 404, message: 'Failed to receive content form api' });
+      } else {
+        console.error(res.response.data);
+        context.error({ statusCode: res.response.status, message: res.response.data });
+      }
+    })
+  }
 }
 </script>
 
-<style>
+<style scoped>
   section {
     @apply pt-header lg:pt-header-lg;
   }
