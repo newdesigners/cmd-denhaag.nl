e32d643dd380e6532b0816fc768fe6ec44e53315
Steven Wu <master_msp1@hotmail.com>
2021-09-21 05:38:00 +0200

    put back client only on top for pop-up

--- components/Popup.vue
+++ components/Popup.vue
@@ -1,25 +1,25 @@
 <template>
-  <div class="popup" :class="{ hidden: isSelected }" v-if="this.$cookies.get('popup') === false || this.$cookies.get('popup') == undefined">
-    <client-only>
-      <div class="container popup__container">
-        <h2 class="popup__title">Choose which programme you’re interested in</h2>
-        <div class="popup__select">
-          <div class="popup__option" @click="setupIXD">
-            <div class="popup__copy">
-              <h3 class="popup__option-title">Communication & Multimedia Design</h3>
-              <p class="popup__option-description">Communication & Multimedia Design (CMD) is a<b>4 year</b> programme taught in <b>Dutch</b>.</p>
+  <client-only>
+    <div class="popup" :class="{ hidden: isSelected }" v-if="this.$cookies.get('popup') === false || this.$cookies.get('popup') == undefined">
+        <div class="container popup__container">
+          <h2 class="popup__title">Choose which programme you’re interested in</h2>
+          <div class="popup__select">
+            <div class="popup__option" @click="setupIXD">
+              <div class="popup__copy">
+                <h3 class="popup__option-title">Communication & Multimedia Design</h3>
+                <p class="popup__option-description">Communication & Multimedia Design (CMD) is a<b>4 year</b> programme taught in <b>Dutch</b>.</p>
+              </div>
+            </div>
+            <div class="popup__option popup__option--right" @click="setupUXD">
+              <div class="popup__copy">
+                <h3 class="popup__option-title">User Experience Design</h3>
+                <p class="popup__option-description">Communication & Multimedia Design - User Experience Design (UXD or CMD - UXD) is a <b>3 year</b> programme taught in <b>English</b>.</p>
+              </div>
             </div>
           </div>
-          <div class="popup__option popup__option--right" @click="setupUXD">
-            <div class="popup__copy">
-              <h3 class="popup__option-title">User Experience Design</h3>
-              <p class="popup__option-description">Communication & Multimedia Design - User Experience Design (UXD or CMD - UXD) is a <b>3 year</b> programme taught in <b>English</b>.</p>
-            </div>
-          </div>
-        </div>
-      </div>  
-    </client-only>  
-  </div>
+        </div>  
+    </div>
+  </client-only>  
 </template>
  
 <script>
