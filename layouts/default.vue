cc7c64ccb09d9d4e291ab8a04ad85e97e65ca0ca
Steven Wu <master_msp1@hotmail.com>
2021-09-29 03:49:21 +0200

    production not working with cookies trying another way

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,14 +11,24 @@ export default {
     return {
       htmlAttrs: {
         lang: this.$store.state.variants.variant === 'IXD' ? 'nl-Nl' : 'en-GB',
-        class: this.$cookies.get('darkmode') ? 'dark' : ''
+        // class: this.$cookies.get('darkmode') ? 'dark' : ''
       }
     }
   },
   mounted() {
-    console.log(this.$cookies.get('darkmode'));
+    const HTML = document.querySelector('html');
+    console.log('darkmode:', this.$cookies.get('darkmode'));
+
     if(this.$cookies.get('darkmode') === undefined) {
       this.$cookies.set('darkmode', false); 
+    }
+
+    if(this.$cookies.get('darkmode') === true) {
+      HTML.classList.add('dark');
+    }
+    
+    if(this.$cookies.get('darkmode') === false) {
+      HTML.classList.remove('dark');
     }
   }
 };
