c521c6b6aee8eaab0ba8409f83641d3632943ad2
Agency <agency@hhsdtp5134simac.wireless.hhs.nl>
2022-03-09 11:48:02 +0100

    embed pdf in page

--- pages/docentendag.vue
+++ pages/docentendag.vue
@@ -1,5 +1,8 @@
 <template>
   <section class="container">
+    <div class="relative overflow-hidden w-full pt-[141%]">
+      <embed width="2480px" height="3508px" class="absolute top-0 left-0 right-0 bottom-0 w-full h-full" src="/pdf/event-schedule.pdf" />
+    </div>
   </section>
 </template>
 <script>
